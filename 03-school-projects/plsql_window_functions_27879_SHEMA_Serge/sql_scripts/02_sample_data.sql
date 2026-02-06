533927ea54e6544522a4204cbaee17243c70e57e
Serge <sajghaa@gmail.com>
2026-02-06 19:50:35 +0200

    Schema_creation screenshot added

---
+++ 03-school-projects/plsql_window_functions_27879_SHEMA_Serge/sql_scripts/02_sample_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO customers (customer_id, first_name, last_name, email, region, signup_date, customer_tier) VALUES
+
+(1, 'Sarah', 'Diana', 'sarah@gmail.com','North America', '2024-01-15','Premium'), 
+(2, 'John', 'Smith', 'johnsmith250@gmail.com','North America', '2024-02-13','Standard'),
+(3, 'Michael', 'Brown', 'michael@gmail.com','North America', '2024-03-10','Premium'),
+(4, 'Sarah', 'Diana', 'sarah@gmail.com','North America', '2024-01-','Standard'),   
