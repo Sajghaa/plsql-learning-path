e22905a49293cc7d3895916c27d8add439a9b204
Serge <sajghaa@gmail.com>
2026-02-18 09:54:18 +0200

    Student Table created with also business rule constraints

--- 03-school-projects/portfolio-window-functions/scenario-1-education/schema/01_business_rules.sql
+++ 03-school-projects/portfolio-window-functions/scenario-1-education/schema/01_business_rules.sql
@@ -19,3 +19,30 @@ DROP SEQUENCE enrollment_seq;
 CREATE SEQUENCE student_seq START WITH 1000 INCREMENT BY 1;
 CREATE SEQUENCE course_seq START WITH 5000 INCREMENT BY 1;
 CREATE SEQUENCE enrollment_seq START WITH 1000 INCREMENT BY 1;
+
+-- TABLE: students
+-- Purpose: Core demographic and status tracking
+-- Business Rule:
+--               - Email must be unique(communication chanel)
+--               - Status limited to defined values (data quality)
+--               - Enrollment date cannot be future (business logic)
+
+
+CREATE TABLE students (
+    student_id NUMBER PRIMARY KEY,
+    first_name VARCHAR2(50) NOT NULL,
+    last_name VARCHAR2(50) NOT NULL,
+    email VARCHAR2(100) UNIQUE NOT NULL,
+    enrollment_date DATE NOT NULL,
+    major VARCHAR2(50),
+    status VARCHAR(20) DEFAULT 'ACTIVE' NOT NULL,
+    last_activity_date DATE,
+    created_date DATE DEFAULT SYSDATE,
+    modified_date DATE,
+
+    -- Business rule constraints
+    CONSTRAINT chk_student_status
+           CHECK (status IN('ACTIVE', 'GRADUATED', 'DROPPED','SUSPENDED')),
+    CONSTRAINT chk_enrollment_date
+           CHECK (enrollment_date <= SYSDATE)
+);
